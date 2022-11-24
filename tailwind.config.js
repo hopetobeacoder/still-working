6510e160cd941088443bb77729be1878b025a93a
Mehmet <teknmehmt@gmail.com>
2022-11-24 14:41:51 +0300

    initialize

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/*  @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './config/*.json',
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid',
+    './src/scss/app.scss',
+    './src/js/app.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
